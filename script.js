a30908d1f3172ab79632049d4ed9ebf6293325bf
Renato Wéssmėr <rewesa@outlook.com.br>
2022-11-28 03:55:15 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+const columns = document.querySelectorAll(".column");
+
+document.addEventListener("dragstart", (e) => {
+    e.target.classList.add("dragging");
+});
+
+document.addEventListener("dragend", (e) => {
+    e.target.classList.remove("dragging");
+});
+
+columns.forEach((item) => {
+    item.addEventListener("dragover", (e) => {
+        const dragging = document.querySelector(".dragging");
+        const applyAfter = getNewPosition(item, e.clienty);
+
+        if (applyAfter) {
+            applyAfter.insertAdjacentElement("afterend", dragging);
+          } else {
+            item.prepend(dragging);
+          }
+    });
+});
+
+function getNewPosition(column, posy) {
+    const cards = column.querySelectorAll(".item:not(.dragging)");
+    let result;
+
+    for (let refer_card of cards) {
+        const box = refer_card.getBoundingClientRect();
+        const boxCentery = box.y + box.height / 2;
+
+        if (posy >= boxCentery) result = refer_card
+    }
+    return result;
+}
